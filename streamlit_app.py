a984bdd6117e903ca4936974ee541b36f3dadcd8
crvstalphua <>
2023-05-17 10:52:49 +0800

    added some requirements

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 import streamlit as st
+from streamlit_chat import message
+from streamlit_extras.colored_header import colored_header
+from streamlit_extras.add_vertical_space import add_vertical_space
+from hugchat import hugchat
 
-st.title('Chatbot for testing')
+st.title('Workpass Chatbot')
 
-st.write('Hello, Im a chatbot meant for testing')
+st.write('A chatbot to answer all your questions')
+
+
+#link: https://crvstalphua-chatbot-test-streamlit-app-ig5flz.streamlit.app/
